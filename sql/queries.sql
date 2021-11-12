0313417d5d6f118619ff75542688978e5aab3f30
dana-liang <71480039+dana-liang@users.noreply.github.com>
2021-11-11 20:20:35 -0600

    Add create view statements

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,40 +1,56 @@
+CREATE VIEW unvaccinated_students
+AS
 SELECT *
 FROM student
 WHERE vaccination_status != 2
 ORDER BY uin;
 
+CREATE VIEW covid_tests_not_positive
+AS
 SELECT * 
 FROM covid_test
 WHERE test_result != 0
 ORDER BY uin;
 
+CREATE VIEW arc_residents
+AS
 SELECT *
 FROM student_housing
 WHERE dorm_id = 'ARC';
 
 -- find percentage frequency of covid test results
+CREATE VIEW test_result_percentages
+AS
 SELECT test_result, ROUND(COUNT(*)*100.0 / (SELECT COUNT(*) FROM covid_test), 2) AS percentage
 FROM covid_test
 GROUP BY test_result;
 
 -- find number of students per organization, filtering out orgs with 1 or fewer members
+CREATE VIEW num_members_per_org
+AS
 SELECT UPPER(organization_name), COUNT(*)
 FROM organization
 GROUP BY organization_id HAVING COUNT(*) >= 2
 
 -- find number of covid tests taken in 2021
+CREATE VIEW num_covid_tests_2021
+AS
 SELECT COUNT(*)
 FROM covid_test
 WHERE YEAR(test_date) = 2021;
 
 --
 
+CREATE VIEW uic_residents_vaccination_status
+AS
 SELECT vaccination_status, first_name, last_name, student.dorm_id, student_housing.room_number
 FROM student
 JOIN student_housing
 ON student.uin = student_housing.uin
 ORDER BY dorm_id, last_name;
 
+CREATE VIEW large_organizations
+AS
 SELECT *
 FROM organization A
 WHERE 2 <=
@@ -42,6 +58,8 @@ WHERE 2 <=
 	 FROM organization
      WHERE organization_id = A.organization_id);
 
+CREATE VIEW unvaccinated_students_in_orgs
+AS
 SELECT *
 FROM organization A
 WHERE EXISTS
